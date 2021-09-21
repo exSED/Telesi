8f356c90fa282c1bc1613d282999354b6da19d1e
Sedriast <sebas@LAPTOP-SB8QR2AU>
2021-09-20 20:53:12 -0500

    LaRePutaExistencia

---
+++ Telesi/Types/Products.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Telesi.Types{
+    public class Products{
+        public string id_ { get; set; }
+        public string name_ { get; set; }
+        public string count_ { get; set; }
+        public string price_ { get; set; }
+    }
+}
