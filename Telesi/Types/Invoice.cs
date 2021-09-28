8b2b94c1c362d80cc00ab06f5dc52661dc3a3708
Sedriast <sebas@LAPTOP-SB8QR2AU>
2021-09-28 08:32:17 -0500

    InitInvoice

---
+++ Telesi/Types/Invoice.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Telesi.Types
+{
+    public class Invoice{
+
+        public string number_ { get; set; }
+        public string date_ { get; set; }
+        public string total_ { get; set; }
+        public List<Products> products_ { get; set; }
+    }
+}
